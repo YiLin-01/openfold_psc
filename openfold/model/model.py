577469779c181058348913170939bf2d7c8f1d33
YiLin-01 <wmulinyi001@gmail.com>
2024-05-29 01:21:25 -0400

    multi debuggings in model.py

--- openfold/model/model.py
+++ openfold/model/model.py
@@ -586,6 +586,7 @@ class AlphaFold(nn.Module):
             outputs["asym_id"] = feats["asym_id"]
 
         # Run auxiliary heads
+        print("self.aux_heads(outputs):", self.aux_heads(outputs))
         outputs.update(self.aux_heads(outputs))
 
         return outputs
